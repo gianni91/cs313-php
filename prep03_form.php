798dab50e90dac32be17ed2faf993502ffa7767c
J Job <jo_ni91@hotmail.com>
2016-01-21 21:54:05 -0700

    trying to pass the rest of the variables

--- prep03_form.php
+++ prep03_form.php
@@ -4,9 +4,9 @@
 
 <p>Testing </p>
 
-Name: <?php echo $_POST["name"]; ?> <br>
-E-mail: <?php echo $_POST["email"]; ?> <br>
-Major: <?php echo $_POST["major"]; ?> <br>
+Name: <?php echo $_POST['name']; ?> <br>
+E-mail: <?php echo $_POST['email']; ?> <br>
+Major: <?php echo $_POST['major']; ?> <br>
 Places Visited: <?php echo $_POST["places"]; ?> <br>
 Comments: <?php echo $_POST["comments"]; ?> <br>
 
