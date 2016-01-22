69a308bbbea33ddd4733a91ae4f8fd63ca53451b
J Job <jo_ni91@hotmail.com>
2016-01-21 23:04:53 -0700

    to show all of the places selected

--- prep03_form.php
+++ prep03_form.php
@@ -6,7 +6,8 @@
 
 Name: <?php echo $_POST['name']; ?> <br>
 E-mail: <?php echo $_POST['email']; ?> <br>
-Major: <?php echo $_POST["major"]; ?> <br>
+Major: <?php echo $_POST['major']; ?> <br>
+
 Comments: <?php echo $_POST["comments"]; ?> <br>
 
 
