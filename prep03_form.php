294d6b05f58ccac09343ee9e36dd8942b4ded868
J Job <jo_ni91@hotmail.com>
2016-01-21 22:06:14 -0700

    to show all of the places selected

--- prep03_form.php
+++ prep03_form.php
@@ -7,7 +7,11 @@
 Name: <?php echo $_POST['name']; ?> <br>
 E-mail: <?php echo $_POST['email']; ?> <br>
 Major: <?php echo $_POST['major']; ?> <br>
-Places Visited: <?php echo $_POST["places"]; ?> <br>
+Places Visited: 
+<?php 
+   foreach($_POST["places"] as $places){
+     echo $places." ";
+}?> <br>
 Comments: <?php echo $_POST["comments"]; ?> <br>
 
 
