fe9572658b41192efb6079d5cb5fbbcbc8ad9e58
J Job <jo_ni91@hotmail.com>
2016-01-21 21:34:52 -0700

    trying to pass the variables

--- prep03_form.php
+++ prep03_form.php
@@ -4,5 +4,15 @@
 
 <p>Testing </p>
 
+<?php if(isset($_POST['submit'])){ 
+$to = "email@example.com"; // this is your Email address 
+$from = $_POST['email']; // this is the sender's Email address 
+$name = $_POST['name']; 
+$comment = $_POST['comments'];
+
+echo $name;
+?>
+
+
 </body>
 </html>
