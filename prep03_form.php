a9ac990cca8dcb5b74598aaa447fe0bb138ab602
J Job <jo_ni91@hotmail.com>
2016-01-21 21:08:46 -0700

    group project php form

---
+++ prep03_form.php
@@ -0,0 +1,28 @@
+<html>
+<body>
+
+Testing
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $major;
+echo "<br>";
+echo $places;
+echo "<br>";
+echo $comment;
+?>
+
+
+
+<?php if(isset($_POST['submit'])){ 
+$to = "email@example.com"; // this is your Email address 
+$from = $_POST['email']; // this is the sender's Email address 
+$name = $_POST['name']; 
+$comment = $_POST['comments'];
+?>
+
+</body>
+</html>
