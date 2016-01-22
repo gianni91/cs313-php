f98e5389492e8f368d9ed8f9171c002fcf4ceb00
J Job <jo_ni91@hotmail.com>
2016-01-21 23:10:30 -0700

    to show all of the places selected

--- prep03_form.php
+++ prep03_form.php
@@ -6,7 +6,21 @@
 
 Name: <?php echo $_POST['name']; ?> <br>
 E-mail: <?php echo $_POST['email']; ?> <br>
-Major: <?php echo $_POST['major']; ?> <br>
+Major: <?php echo $_POST["major"]; ?> <br>
+Places Visited:
+<?php
+if (isset($_POST['submit'])) {
+if(!empty($_POST['places'])) {
+echo "<span>You have selected :</span><br/>";
+// As output of $_POST['places'] is an array we have to use Foreach Loop to display individual value
+foreach ($_POST['places'] as $select)
+{
+echo "<span><b>".$select."</b></span><br/>";
+}
+}
+else { echo "<span>Please Select Atleast One Color.</span><br/>";}
+}
+?>
 
 Comments: <?php echo $_POST["comments"]; ?> <br>
 
