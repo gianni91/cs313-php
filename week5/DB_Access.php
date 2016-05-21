aa420aabff35f542d95501c54ee601da3d4adf46
Jonny <jo_ni91@hotmail.com>
2016-05-21 03:36:32 -0600

    trying to echo some data from the database

--- week5/DB_Access.php
+++ week5/DB_Access.php
@@ -1,12 +1,26 @@
 <?php
-$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-$dbName = "Students";
+try
+{
+	$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+	$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+	$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	$dbName = "Students";
 
-echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
+	$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+	echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
+}
+catch (PDOException $ex)
+{
+	echo 'Error!: ' . $ex->getMesage();
+	die();
+}
 
-$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+foreach ($db->query('SELECT name, age FROM Students') as $row)
+{
+   echo 'name: ' . $row['name'];
+   echo ' age: ' . $row['age'];
+   echo '<br />';
+}
 
 ?>
