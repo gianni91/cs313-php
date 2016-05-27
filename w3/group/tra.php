b530320a3b61d9dc2fd4cbc8de5626a780df7686
Jonny <jo_ni91@hotmail.com>
2016-05-26 18:38:38 -0600

    putting all Web2 stuff in 1 file

--- w3/group/tra.php
+++
@@ -1,9 +0,0 @@
-<!-- This is just a file im using to do some testing with... -->
-<html>
-<body>
-
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-
-</body>
-</html>
