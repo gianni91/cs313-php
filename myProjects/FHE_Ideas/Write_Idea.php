12ed19823ad11ac1a0276491a6d75a14301f0b83
Jonny <jo_ni91@hotmail.com>
2016-05-26 18:06:18 -0600

    cleanup

--- myProjects/FHE_Ideas/Write_Idea.php
+++
@@ -1,9 +0,0 @@
-<?php
-	header("Content-type: text/xml\n\n");
-	echo "success"; 
-       	$text = $_POST["passText"];
-       	$ideaFile = fopen("FHE_Ideas.txt", "w") or die("failed to write to file");
-       	fwrite($ideaFile, $text);
-       	fclose($ideaFile);
-
-?>
